b80a39e581da269491a54af4d4e27b7c7ea7e9cf
Jens-Kristian Krogager <jens-kristian@krogager.dk>
2021-01-27 11:12:10 +0100

    Implementing logging and error handling

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ Automatically Classify and Reduce a given Data Set
 
 from argparse import ArgumentParser
 import os
+import datetime
 
 from . import alfosc
 from . import data_organizer as do
@@ -15,21 +16,94 @@ v_file = os.path.join(code_dir, 'VERSION')
 with open(v_file) as version_file:
     __version__ = version_file.read().strip()
 
+
+class Report(object):
+    def __init__(self):
+        self.time = datetime.datetime.now()
+        self.fname = 'pynot_%s.log' % self.time.strftime('%d%b%Y-%Hh%Mm%S')
+        self.remarks = list()
+        self.lines = list()
+        self.header = """
+        #  PyNOT Data Processing Pipeline
+        # ================================
+        # version %s
+        %s
+
+        """ % (__version__, self.time.strftime("%b %d, %Y  %H:%M:%S"))
+        self.report = ""
+
+    def commit(self, text):
+        self.lines.append(text)
+
+    def error(self, text):
+        text = ' [ERROR]  - ' + text
+        if text[-1] != '\n':
+            text += '\n'
+        self.lines.append(text)
+
+    def warn(self, text):
+        text = '[WARNING] - ' + text
+        if text[-1] != '\n':
+            text += '\n'
+        self.lines.append(text)
+
+    def write(self, text):
+        text = '          > ' + text
+        if text[-1] != '\n':
+            text += '\n'
+        self.lines.append(text)
+
+    def add_linebreak(self):
+        self.lines.append("\n")
+
+    def add_remark(self, text):
+        self.remarks.append(text)
+
+    def _make_report(self):
+        remark_str = ''.join(self.remarks)
+        lines_str = ''.join(self.lines)
+        self.report = '\n'.join([self.header, remark_str, lines_str])
+
+    def print_report(self):
+        self._make_report()
+        print(self.report)
+
+    def save(self):
+        self._make_report()
+        with open(self.fname, 'w') as output:
+            output.write(self.report)
+
+
 def main(raw_path, verbose=True):
-    if os.path.exists(raw_path):
-        pass
-    else:
-        return False
+    log = Report()
+
+    if not os.path.exists(raw_path):
+        log.error("Data path does not exist : %s" % raw_path)
+        log.write("Pipeline terminated!")
+        return
 
     # Classify files:
-    collection = do.classify(raw_path, progress=verbose)
-    database = do.TagDatabase(collection)
+    log.write("Classyfying files in folder: %s" % raw_path)
+    try:
+        collection, message = do.classify(raw_path, progress=verbose)
+        database = do.TagDatabase(collection)
+        log.commit(message)
+    except ValueError as err:
+        log.error(str(err))
+        print(err)
+    except FileNotFoundError as err:
+        log.error(str(err))
+        print(err)
+    finally:
+        log.save()
+        return
 
     # -- Organize object files in dataset:
     object_filelist = database['SPEC_OBJECT']
     object_images = list(map(do.RawImage, object_filelist))
     if verbose:
         print("\n - The following objects were found in the dataset:")
+        print("OBJECT                  GRISM       SLIT      EXPTIME")
         for sci_img in object_images:
             print("%20s  %9s  %11s  %.0f" % (sci_img.object, sci_img.grism, sci_img.slit, sci_img.exptime))
         print("")
