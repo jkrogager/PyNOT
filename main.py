234e4cc53829cb8a6b98a90d597620c97cc63687
Jens-Kristian Krogager <jens-kristian@krogager.dk>
2021-01-27 14:59:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 Automatically Classify and Reduce a given Data Set
 """
 
+from astropy.io import fits
 from argparse import ArgumentParser
+from collections import defaultdict
 import os
 import datetime
+import yaml
 
 from . import alfosc
 from . import data_organizer as do
@@ -12,13 +15,15 @@ from .calibs import combine_bias_frames, combine_flat_frames, normalize_spectral
 
 code_dir = os.path.dirname(os.path.abspath(__file__))
 calib_dir = os.path.join(code_dir, '/calib/')
+defaults_fname = os.path.join(calib_dir, 'default_options.yml')
 v_file = os.path.join(code_dir, 'VERSION')
 with open(v_file) as version_file:
     __version__ = version_file.read().strip()
 
 
 class Report(object):
-    def __init__(self):
+    def __init__(self, verbose=False):
+        self.verbose = verbose
         self.time = datetime.datetime.now()
         self.fname = 'pynot_%s.log' % self.time.strftime('%d%b%Y-%Hh%Mm%S')
         self.remarks = list()
@@ -32,28 +37,41 @@ class Report(object):
         """ % (__version__, self.time.strftime("%b %d, %Y  %H:%M:%S"))
         self.report = ""
 
+        if self.verbose:
+            print(self.header)
+
     def commit(self, text):
+        if self.verbose:
+            print(text)
         self.lines.append(text)
 
     def error(self, text):
         text = ' [ERROR]  - ' + text
+        if self.verbose:
+            print(text)
         if text[-1] != '\n':
             text += '\n'
         self.lines.append(text)
 
     def warn(self, text):
         text = '[WARNING] - ' + text
+        if self.verbose:
+            print(text)
         if text[-1] != '\n':
             text += '\n'
         self.lines.append(text)
 
-    def write(self, text):
-        text = '          > ' + text
+    def write(self, text, prefix='          > '):
+        text = prefix + text
+        if self.verbose:
+            print(text)
         if text[-1] != '\n':
             text += '\n'
         self.lines.append(text)
 
     def add_linebreak(self):
+        if self.verbose:
+            print("")
         self.lines.append("\n")
 
     def add_remark(self, text):
@@ -73,18 +91,36 @@ class Report(object):
         with open(self.fname, 'w') as output:
             output.write(self.report)
 
+    def fatal_error(self):
+        print("!! FATAL ERROR !!")
+        print("Consult the log: %s\n" % self.fname)
+        self.save()
 
-def main(raw_path, dataset=None, verbose=True):
-    log = Report()
+
+def get_options(option_fname):
+    with open(option_fname) as opt_file:
+        options = yaml.full_load(opt_file)
+    return options
+
+
+def main(raw_path, options_fname=None, verbose=True):
+    log = Report(verbose)
+
+    # -- Parse Options from YAML
+    options = get_options(defaults_fname)
+    if options_fname:
+        user_options = get_options(options_fname)
+        options.update(user_options)
 
     if not os.path.exists(raw_path):
         log.error("Data path does not exist : %s" % raw_path)
-        log.write("Pipeline terminated!")
+        log.fatal_error()
         return
 
+    dataset = options['dataset']
     if dataset is not None:
         # -- load collection
-        # -- check that all files in raw_path are in collection
+        # -- reclassify (takes already identified files into account)
         pass
 
     # Classify files:
@@ -93,78 +129,82 @@ def main(raw_path, dataset=None, verbose=True):
         database, message = do.classify(raw_path, progress=verbose)
         # -- Save collection
         log.commit(message)
+        log.add_linebreak()
     except ValueError as err:
         log.error(str(err))
         print(err)
+        log.fatal_error()
+        return
     except FileNotFoundError as err:
         log.error(str(err))
-        print(err)
-    finally:
-        log.save()
+        log.fatal_error()
         return
 
     # -- Organize object files in dataset:
     object_filelist = database['SPEC_OBJECT']
     object_images = list(map(do.RawImage, object_filelist))
-    if verbose:
-        print("\n - The following objects were found in the dataset:")
-        print("OBJECT                  GRISM       SLIT      EXPTIME")
-        for sci_img in object_images:
-            print("%20s  %9s  %11s  %.0f" % (sci_img.object, sci_img.grism, sci_img.slit, sci_img.exptime))
-        print("")
 
-    # -- Check ARC:
+    log.add_linebreak()
+    log.write(" - The following objects were found in the dataset:", prefix='')
+    log.write("      OBJECT          GRISM       SLIT      EXPTIME", prefix='')
+    for sci_img in object_images:
+        log.write("%20s  %9s  %11s  %.0f" % (sci_img.object, sci_img.grism, sci_img.slit, sci_img.exptime), prefix='')
+    log.add_linebreak()
+
+    # get list of unique grisms in dataset:
+    grism_list = list()
+    for sci_img in object_images:
+        grism_name = alfosc.grism_translate[sci_img.grism]
+        if grism_name not in grism_list:
+            grism_list.append(grism_name)
+
+    # -- Check arc line files:
     arc_images = list()
     for arc_type in ['ARC_He', 'ARC_HeNe', 'ARC_Ne', 'ARC_ThAr']:
         if arc_type in database.keys():
             arc_images += database[arc_type]
 
-    if identify_interactive and identify_all:
-        pass
-    else:
-        # identify all grisms (identify_all==FALSE)
-        # identify only missing grisms (identify_interactive==FALSE)
+    arc_images_for_grism = defaultdict(list)
+    for arc_img in arc_images:
+        raw_grism = fits.getheader(arc_img)['ALGRNM']
+        this_grism = alfosc.grism_translate[raw_grism]
+        arc_images_for_grism[this_grism].append(arc_img)
 
-        # get list of unique grisms in dataset:
-        grism_list = list()
-        for sci_img in object_images:
-            grism_name = alfosc.grism_translate[sci_img.grism]
-            if grism_name not in grism_list:
-                grism_list.append(grism_name)
-
-        # Check if pixeltable exists:
-        if identify_interactive:
-            # Make pixeltable for all grisms:
-            grisms_to_identify = grism_list
+    for grism_name in grism_list:
+        if len(arc_images_for_grism[grism_name]) == 0:
+            log.error("No arc frames defined for grism: %s" % grism_name)
+            log.fatal_error()
+            return
         else:
-            grisms_to_identify = list()
-            for grism_name in grism_list:
-                pixtab_fname = calib_dir + '/%s_pixeltable.dat' % grism_name
-                if not os.path.exists(pixtab_fname):
-                    grisms_to_identify.append(grism_name)
+            log.write("%s has necessary arc files." % grism_name)
 
-            if len(grisms_to_identify) == 0:
-                pass
+    log.add_linebreak()
+    identify_all = options['identify']['all']
+    identify_interactive = options['identify']['interactive']
+    if identify_interactive and identify_all:
+        log.write("Identify: interactively reidentify arc lines for all objects")
+    elif identify_interactive and not identify_all:
+        # Make pixeltable for all grisms:
+        grisms_to_identify = grism_list
+        log.write("Identify: interactively reidentify all grisms in dataset:")
+        log.write(", ".join(grisms_to_identify))
+    else:
+        # Check if pixeltables exist:
+        grisms_to_identify = list()
+        for grism_name in grism_list:
+            pixtab_fname = calib_dir + '/%s_pixeltable.dat' % grism_name
+            if not os.path.exists(pixtab_fname):
+                grisms_to_identify.append(grism_name)
+                log.write("%s : pixel table does not exist. Will identify lines..." % grism_name)
             else:
-                print("\n - The following grisms are used in the dataset")
-                print("   but no pixeltable exists in the calibration database:")
-            for grism_name in grisms_to_identify:
-                print(grism_name)
+                log.write("%s : pixel table already exists" % grism_name)
+        log.add_linebreak()
 
-        print("\n   Starting interactive definition of pixeltable...")
-        print("   Get your line identification plot ready!")
-        for grism_name in grisms_to_identify:
-            arc_images_for_grism = list()
-            for arc_img in arc_images:
-                this_grism = alfosc.grism_translate[arc_img.grism]
-                if this_grism == grism_name:
-                    arc_images_for_grism.append(arc_img)
+    if len(grisms_to_identify) > 0:
+        log.write("Starting interactive definition of pixel table...")
 
-            if len(arc_images_for_grism) == 0:
-                print("[ERROR] - No arc frames defined for grism: " + grism_name)
-                return None
-            else:
-                create_pixtable(arc_images_for_grism, grism_name)
+    for grism_name in grisms_to_identify:
+        create_pixtable(arc_images_for_grism, grism_name)
 
     for sci_img in object_images:
         output_dir = sci_img.target_name
@@ -206,6 +246,27 @@ def main(raw_path, dataset=None, verbose=True):
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("path", type=str,
+    parser.add_argument("path", type=str, nargs='?', default='',
                         help="Path to directory containing the raw data")
-    main(parser.path)
+    parser.add_argument("--options", type=str, default='',
+                        help="Filename of options in YAML format")
+    args = parser.parse_args()
+
+    if args.path:
+        main(args.path, options_fname=args.options)
+
+    else:
+        print("\n  Running PyNOT Data Processing Pipeline\n")
+        if not os.path.exists('options.yml'):
+            copy_cmd = "cp %s options.yml" % defaults_fname
+            os.system(copy_cmd)
+            print(" - Created the default option file: options.yml")
+        message = """
+         - Update the file and run PyNOT as:
+            %] pynot --options options.yml
+
+        Otherwise provide a path to the raw data:
+            %] pynot path/to/data
+
+        """
+        print(message)
