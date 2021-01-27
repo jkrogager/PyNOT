847b4500df7e9e667dde6699bf1e0b505f387a8b
Jens-Kristian Krogager <jens-kristian@krogager.dk>
2021-01-27 11:06:21 +0100

    cleanup and tests

--- __init__.py
+++ __init__.py
@@ -11,13 +11,13 @@ v_file = os.path.join(code_dir, 'VERSION')
 with open(v_file) as version_file:
     __version__ = version_file.read().strip()
 
-print ""
-print "   PyNOT  v. %r " % __version__
-print ""
-print "   Data Reduction Tools for ALFOSC"
-print "   Mounted at the Nordic Optical Telescope"
-print ""
-print "   Written by Jens-Kristian Krogager"
-print "   Institut d'Astrophysique de Paris"
-print "   March 2017"
-print ""
+print("")
+print("   PyNOT  v. %r " % __version__)
+print("")
+print("   Data Reduction Tools for ALFOSC")
+print("   Mounted at the Nordic Optical Telescope")
+print("")
+print("   Written by Jens-Kristian Krogager")
+print("   Institut d'Astrophysique de Paris")
+print("   March 2017")
+print("")
